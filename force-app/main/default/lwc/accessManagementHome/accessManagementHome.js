d911e72280a2008d3bf368d074691552dc0aa5b0
John Arne Føreland <john.foreland@me.com>
2020-07-18 12:09:37 +0200

    renamed components

--- force-app/main/default/lwc/accessManagementHome/accessManagementHome.js
+++ force-app/main/default/lwc/accessManagementHome/accessManagementHome.js
@@ -1,3 +1,58 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import AccessManagementImages from '@salesforce/resourceUrl/AccessManagementImages';
 
-export default class AccessManagementHome extends LightningElement {}
+export default class AccessManagementHome extends NavigationMixin(LightningElement) {
+
+    data = [
+        {
+            "id": "apply",
+            "img": AccessManagementImages + '/apply.png',
+            "text": "Søk tilgang",
+            "func": this.apply,
+            "style": "background-color: #b8ebf9;"
+        },
+        {
+            "id": "access",
+            "img": AccessManagementImages + '/access.png',
+            "text": "Se tilganger",
+            "func": this.access,
+            "style": "background-color: #8bd2ad;"
+        },
+        {
+            "id": "applications",
+            "img": AccessManagementImages + '/applications.png',
+            "text": "Se søknader",
+            "func": this.applications,
+            "style": "background-color: #ffd290;"
+        }
+    ];
+
+
+    apply(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__navItemPage',
+            attributes: {
+                apiName: 'AccessApplication',
+            }
+        });
+    }
+
+    access(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__navItemPage',
+            attributes: {
+                apiName: this.tabName,
+            }
+        });
+    }
+
+    applications(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__navItemPage',
+            attributes: {
+                apiName: this.tabName,
+            }
+        });
+    }
+}
